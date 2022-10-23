65dec3e27e0bfd71d4b10793d02fd22e598500bc
dev-eunjiLee <dev.eunji.lee@gmail.com>
2022-10-23 12:22:59 +0900

    END: (42842) 카펫 2. 2차 방정식 없이 완료

--- Level_2/42842.js
+++ Level_2/42842.js
@@ -20,5 +20,38 @@ function solution(brown, yellow) {
   return [x, y];
 }
 
-console.log(solution(10, 2));
-console.log(solution(8, 1));
+// console.log(solution(10, 2));
+// console.log(solution(8, 1));
+
+function solutionNoEquation(brown, yellow) {
+  console.log(brown, yellow);
+
+  const minimumWidth = 3;
+  let width = minimumWidth;
+
+  let height;
+  while (true) {
+    console.log(width);
+
+    height = (brown + yellow) / width;
+    if (
+      // 높이가 정수인지 확인
+      Number.isInteger(height) === true &&
+      // 높이가 너비보다 작은지 확인
+      height <= width &&
+      // 각각 yellow, brown 조건 맞는지 확인
+      (width - 2) * (height - 2) === yellow &&
+      2 * width + 2 * (height - 2) === brown
+    ) {
+      break;
+    }
+
+    width++;
+  }
+
+  return [width, height];
+}
+
+console.log(solutionNoEquation(10, 2));
+console.log(solutionNoEquation(8, 1));
+console.log(solutionNoEquation(18, 6));
