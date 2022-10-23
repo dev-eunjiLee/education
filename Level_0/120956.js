f0a4577771b398560940555a81a8d55ff568fb8a
dev-eunjiLee <dev.eunji.lee@gmail.com>
2022-10-23 10:49:38 +0900

    END: (120958) 옹알이 수정 완료

--- Level_0/120956.js
+++ Level_0/120956.js
@@ -1,32 +1,24 @@
 function solution(babbling) {
-  console.log(babbling);
   const babyWordList = ["aya", "ye", "woo", "ma"];
 
-  const cnt = babbling.filter((per) => {
+  return babbling.filter((per) => {
     let keyword = per;
 
     for (const perBabyWord of babyWordList) {
-      console.log(`현재 키워드: "${keyword}"`);
+      // 1. regex 생성
       const regex = new RegExp(perBabyWord, "g");
-      console.log(regex);
 
+      // 2. regex에 걸리는 키워드를 "#"으로 변경
       keyword = keyword.replace(regex, "#");
-      console.log(`변경된 키워드: "${keyword}"`);
 
+      // 3-1. "#"이 연속으로 있는 경우 애기가 발음할 수 없는 예외 단어이기 때문에 break
       if (keyword.match(/#{2,}/g) !== null) {
         break;
-      } else {
-        keyword = keyword.replace(/#/g, "*");
       }
-      console.log(`다시 변경된 키워드: "${keyword}"`);
+      // 3-2. 애기가 발음할 수 있는 경우 "#"을 "*"으로 변경(아예 없애버리면 다음 키워드 regex 검색을 할 때 떨어져있는 단어인데도 붙어있는 걸로 처리되어 오류가 날 수 있음)
+      keyword = keyword.replace(/#/g, "*");
     }
 
-    return keyword.match(/#{2,}/g) === null || keyword.match(/\*+/) !== null;
+    return keyword.match(/[^*]/g) === null;
   }).length;
-  console.log(`개수: ${cnt}`);
-  return cnt;
 }
-
-console.log(solution(["aya", "yee", "u", "maa"]));
-console.log(solution(["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"]));
-console.log(solution(["ayayewoomawooma"]));
