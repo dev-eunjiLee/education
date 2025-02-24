a1abca978f88f4082e740b2e0cf51065017b6dd6
dev-eunjiLee <dev.eunji.lee@gmail.com>
2025-02-24 22:29:36 +0900

    FIX: (Lv.o 181940) repeat 함수 적용

--- Level_0/181940.js
+++ Level_0/181940.js
@@ -1,5 +1,6 @@
 function solution(my_string, k) {
-  return Array.from({ length: k }, (_, i) => my_string).join("");
+  //   return Array.from({ length: k }, (_, i) => my_string).join("");
+  return my_string.repeat(k);
 }
 
 console.log(solution("abc", 3));
