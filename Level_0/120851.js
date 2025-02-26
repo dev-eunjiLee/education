34cdc5345c86ddf11d8341daa1a8ae9878a44762
dev-eunjiLee <dev.eunji.lee@gmail.com>
2025-02-26 17:05:24 +0900

    FIX: (Lv.0 120851) string도 전개연산자 사용 가능

--- Level_0/120851.js
+++ Level_0/120851.js
@@ -1,6 +1,7 @@
 function solution(my_string) {
   let sum = 0;
-  for (const elem of my_string.split("")) {
+  //   for (const elem of my_string.split("")) {
+  for (const elem of [...my_string]) {
     const convertedElem = Number(elem);
     if (!isNaN(convertedElem)) {
       sum += convertedElem;
