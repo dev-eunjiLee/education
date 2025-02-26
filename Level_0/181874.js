27b2803e884f404b38ba33740f599cb3ad084e86
dev-eunjiLee <dev.eunji.lee@gmail.com>
2025-02-26 21:55:50 +0900

    ADD: (Lv.0 181874) 로직 간소화

--- Level_0/181874.js
+++ Level_0/181874.js
@@ -1,8 +1,9 @@
 function solution(myString) {
-  return myString.replaceAll(/[a-zA-Z]/g, (match) => {
-    if (match === "a" || match === "A") return match.toUpperCase();
-    else return match.toLowerCase();
-  });
+  //   return myString.replaceAll(/[a-zA-Z]/g, (match) => {
+  //     if (match === "a" || match === "A") return match.toUpperCase();
+  //     else return match.toLowerCase();
+  //   });
+  return myString.toLowerCase().replaceAll("a", "A");
 }
 
 console.log(solution("abstract algebra"));
