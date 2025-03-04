47769c4fa10e7e3a21e42c984605f041885269b8
dev-eunjiLee <dev.eunji.lee@gmail.com>
2025-03-04 15:16:50 +0900

    ADD: (Lv.0 120838) 모스부호 (1)

---
+++ Level_0/120838.js
@@ -0,0 +1,38 @@
+function solution(letter) {
+  const morse = {
+    ".-": "a",
+    "-...": "b",
+    "-.-.": "c",
+    "-..": "d",
+    ".": "e",
+    "..-.": "f",
+    "--.": "g",
+    "....": "h",
+    "..": "i",
+    ".---": "j",
+    "-.-": "k",
+    ".-..": "l",
+    "--": "m",
+    "-.": "n",
+    "---": "o",
+    ".--.": "p",
+    "--.-": "q",
+    ".-.": "r",
+    "...": "s",
+    "-": "t",
+    "..-": "u",
+    "...-": "v",
+    ".--": "w",
+    "-..-": "x",
+    "-.--": "y",
+    "--..": "z",
+  };
+
+  return letter.split(" ").reduce((prev, curr) => {
+    // console.log(prev, curr);
+    // console.log(morse[curr]);
+    return (prev += morse[curr]);
+  }, "");
+}
+
+console.log(solution(".... . .-.. .-.. ---")); // hello
