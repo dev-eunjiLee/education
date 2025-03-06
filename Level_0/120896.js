26f003c9b66ea856406a4b21e0b2c4ebe9525962
dev-eunjiLee <dev.eunji.lee@gmail.com>
2025-03-06 21:42:20 +0900

    ADD: (Lv.0 120896) 한 번만 등장한 문자

---
+++ Level_0/120896.js
@@ -0,0 +1,15 @@
+function solution(s) {
+  const obj = s.split("").reduce((prev, curr) => {
+    prev[curr] = prev?.[curr] === undefined ? 1 : prev[curr] + 1;
+    return prev;
+  }, {});
+  return Object.keys(obj)
+    .filter((key) => {
+      return obj[key] === 1;
+    })
+    .sort()
+    .join("");
+}
+
+console.log(solution("hello")); // eho
+console.log(solution("abcabcadc")); // d
