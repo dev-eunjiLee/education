3e05016cbf7789eb05cf691555551fb76dc5598d
dev-eunjiLee <dev.eunji.lee@gmail.com>
2025-03-11 21:49:22 +0900

    FIX: (Lv.0 120869) 재귀함수 적용

--- Level_0/120869.js
+++ Level_0/120869.js
@@ -14,8 +14,39 @@ function solution(spell, dic) {
   return answer;
 }
 
-console.log(solution(["p", "o", "s"], ["sod", "eocd", "qixm", "adio", "soo"])); //2
-console.log(solution(["z", "d", "x"], ["def", "dww", "dzx", "loveaw"])); //1
+// console.log(solution(["p", "o", "s"], ["sod", "eocd", "qixm", "adio", "soo"])); //2
+// console.log(solution(["z", "d", "x"], ["def", "dww", "dzx", "loveaw"])); //1
+// console.log(
+//   solution(["s", "o", "m", "d"], ["moos", "dzx", "smm", "sunmmo", "som"])
+// ); //2
+
+function solution2(spell, dic) {
+  const generateWord = (arr, prefix = "", result = new Set()) => {
+    if (arr.length !== 0) {
+      for (let i = 0; i < arr.length; i++) {
+        const newArr = arr.slice(0, i).concat(arr.slice(i + 1));
+        generateWord(newArr, `${prefix}${arr[i]}`, result);
+      }
+    } else {
+      result.add(prefix);
+    }
+    return result;
+  };
+
+  const wordSet = generateWord(spell);
+
+  let answer = 2;
+  for (const elem of Array.from(wordSet)) {
+    if (dic.includes(elem)) {
+      answer = 1;
+      break;
+    }
+  }
+  return answer;
+}
+
+console.log(solution2(["p", "o", "s"], ["sod", "eocd", "qixm", "adio", "soo"])); //2
+console.log(solution2(["z", "d", "x"], ["def", "dww", "dzx", "loveaw"])); //1
 console.log(
-  solution(["s", "o", "m", "d"], ["moos", "dzx", "smm", "sunmmo", "som"])
+  solution2(["s", "o", "m", "d"], ["moos", "dzx", "smm", "sunmmo", "som"])
 ); //2
