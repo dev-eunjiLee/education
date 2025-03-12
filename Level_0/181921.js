f7ee4aaa253152ea3b9aeb119b856f490e4b5151
dev-eunjiLee <dev.eunji.lee@gmail.com>
2025-03-12 23:37:08 +0900

    FIX: (Lv.0 181921) 제너레이터 적용

--- Level_0/181921.js
+++ Level_0/181921.js
@@ -11,3 +11,31 @@ function solution(l, r) {
 
 console.log(solution(5, 555)); //[5, 50, 55, 500, 505, 550, 555]
 console.log(solution(10, 20)); //[-1]
+
+function* generate() {
+  let i = 0;
+  while (true) {
+    yield Number(Number(i).toString(2)) * 5;
+    i++;
+  }
+}
+
+function solution2(l, r) {
+  const gen = generate();
+
+  let a = 0;
+
+  const arr = [];
+
+  while (a < l) {
+    a = gen.next().value;
+  }
+  while (a <= r) {
+    arr.push(a);
+    a = gen.next().value;
+  }
+  return arr.length ? arr : [-1];
+}
+
+console.log(solution2(5, 555)); //[5, 50, 55, 500, 505, 550, 555]
+console.log(solution2(10, 20)); //[-1]
