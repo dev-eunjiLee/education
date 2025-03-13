f01206bc2484c4d0b1639de286a0c06b5ac21228
dev-eunjiLee <dev.eunji.lee@gmail.com>
2025-03-13 19:45:06 +0900

    FIX: (Lv.1 77884) 제곱근이 정수면 약수의 개수가 홀수다

--- Level_1/77884.js
+++ Level_1/77884.js
@@ -19,3 +19,21 @@ function solution(left, right) {
 // console.log(solution(13, 17)); //43
 // console.log(solution(24, 27)); //52
 console.log(solution(1, 2)); // 1
+
+function solution2(left, right) {
+  let answer = 0;
+  for (let i = left; i <= right; i++) {
+    // Math.sqrt(n) >> n의 제곱근을 리턴
+    // 제곱근이 정수면 약수의 개수가 홀수다
+    if (Number.isInteger(Math.sqrt(i))) {
+      answer -= i;
+    } else {
+      answer += i;
+    }
+  }
+  return answer;
+}
+
+console.log(solution2(13, 17)); //43
+console.log(solution2(24, 27)); //52
+console.log(solution2(1, 2)); // 1
