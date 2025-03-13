9d417f69f9ea50141d53d034253c8a6067cdbf4d
dev-eunjiLee <dev.eunji.lee@gmail.com>
2025-03-13 19:40:32 +0900

    ADD: (Lv.1 77884) 약수의 개수와 덧셈

--- Level_1/77884.js
+++ Level_1/77884.js
@@ -1,42 +1,21 @@
-/**
- * @link https://school.programmers.co.kr/learn/courses/30/lessons/77884
- * @description
- * 두 정수 left와 right가 매개변수로 주어집니다.
- * left부터 right까지의 모든 수들 중에서, 약수의 개수가 짝수인 수는 더하고, 약수의 개수가 홀수인 수는 뺀 수를 return 하도록 solution 함수를 완성해주세요.
- */
 function solution(left, right) {
-	let answer = 0;
+  let sum = 0;
+  for (let elem = left; elem <= right; elem++) {
+    if (elem === 1) sum -= 1;
+    else if (elem === 2) sum += 2;
+    else {
+      let cnt = 2; // 1, elem
+      for (let i = 2; i < elem; i++) {
+        if (elem % i === 0) cnt++;
+      }
+      if (cnt % 2 === 0) sum += elem;
+      else sum -= elem;
+    }
+  }
 
-	const calculationList = [];
-
-	for (let mainIndex = left; mainIndex <= right; mainIndex++) {
-		console.log(mainIndex, right);
-
-		// divisor: 약수
-		let divisorCnt = 0;
-
-		if (mainIndex === 16) {
-			console.log(3);
-		}
-
-		for (let i = 1; i <= mainIndex; i++) {
-			console.log(`✅ ${i}, ${mainIndex}`);
-			if (mainIndex % i === 0) {
-				divisorCnt++;
-			}
-		}
-
-		if (divisorCnt % 2 !== 0) {
-			calculationList.push(mainIndex * -1);
-		} else {
-			calculationList.push(mainIndex);
-		}
-	}
-
-	console.log(calculationList);
-
-	return answer;
+  return sum;
 }
 
-console.log(solution(13, 17)); // 43
-console.log(solution(24, 27)); // 52
+// console.log(solution(13, 17)); //43
+// console.log(solution(24, 27)); //52
+console.log(solution(1, 2)); // 1
