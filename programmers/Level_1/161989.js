06fe6c15fc746a637525ca91db1c02953debcb9e
dev-eunjiLee <dev.eunji.lee@gmail.com>
2025-03-21 16:17:07 +0900

    FIX: (Lv.1 1619189) 배열 포함 로직을 대소비교로 변경

--- programmers/Level_1/161989.js
+++ programmers/Level_1/161989.js
@@ -1,18 +1,13 @@
 function solution(n, m, section) {
-  // left 변수를 section에 포함된 값을 만날 때 까지 +1하고, 만나는 경우, m길이만큼 left를 추가한다. 이 과정을 left가 section을 벗어날 때 까지 진행한다.
-  let left = 0;
   let cnt = 0;
-
-  while (left <= n - 1) {
-    if (!section.includes(left + 1)) {
-      left++;
-      continue;
+  let lastPaintedPoint = 0;
+  // section을 기준으로 배열을 돌린다.
+  for (const point of section) {
+    if (lastPaintedPoint < point) {
+      cnt++;
+      lastPaintedPoint = point + m - 1;
     }
-
-    cnt++;
-    left = left + m;
   }
-
   return cnt;
 }
 
