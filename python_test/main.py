73ac44489082b808a495a18d7b0e9854ca7fffb1
dev-eunjiLee <dev.eunji.lee@gmail.com>
2025-06-29 16:24:33 +0900

    feat: 스터디용 파이썬 코드에 docstring 추가(pylint), 파라미터명 수정

--- python_test/main.py
+++ python_test/main.py
@@ -1,3 +1,7 @@
+"""
+main.py 이 모듈은 광고 이벤트 트래킹 로직을 구현
+"""
+
 from enum import Enum
 from dataclasses import dataclass
 from dataclasses import asdict
@@ -5,6 +9,7 @@ from dataclasses import asdict
 
 # Enum > 클래스의 파라미터로 Enum을 넣어주면 생성 가능
 class EventType(Enum):
+    """이벤트의 타입"""
     IMPRESSION = 'IMPRESSION'
     CLICK = 'CLICK'
     CONVERSATION = 'CONVERSATION'
@@ -12,13 +17,16 @@ class EventType(Enum):
 
 @dataclass
 class Event:
+    """실제 발생하는 이벤트의 정보를 기입"""
     type: EventType
     user_id: str
     ad_id: str
 
 
 class Redis():
-    def _insert(self, event: Event) -> bool:
+    """(임의) Redis"""
+    def insert(self, event: Event) -> bool:
+        """Redis에 데이터를 적재하는 상황을 임의 구현(로그)"""
         # 실제 레디스 저장 로직 대신에 로그로 구현
         print('Redis._insert()', event)
 
@@ -28,7 +36,8 @@ class Redis():
 r = Redis()
 
 
-def track(body: Event) -> bool:
+def track(event: Event) -> bool:
+    """파라미터로 받은 Event를 트래킹"""
 
     # 검증을 위한 딕셔너리 선언
     expected_types = {
@@ -38,8 +47,8 @@ def track(body: Event) -> bool:
     }
 
     # 검증(현재는 타입 체크만 진행함)
-    for key in asdict(body).keys():
-        value = getattr(body, key)  # getattr: 객체에서 변수 접근하기
+    for key in asdict(event).keys():
+        value = getattr(event, key)  # getattr: 객체에서 변수 접근하기
         expected_type = expected_types[key]  # dict[key]: 딕셔너리에서 변수 접근하기
 
         print({'key': key, 'expected_type': expected_type})
@@ -48,7 +57,7 @@ def track(body: Event) -> bool:
             raise TypeError(f'event의 {key}은/는 {expected_type}이어야합니다.')
 
     # 저장
-    result = r._insert(body)
+    result = r.insert(event)
 
     return result
 
